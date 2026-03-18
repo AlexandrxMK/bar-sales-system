7cc2fd531ded146b16757039093d74ef13eeb3f5
Alexandre <alexandre.guedes@academico.ufpb.br>
2026-03-17 22:49:37 -0300

    Corrigir troca de nome e marca na classe Produto e exibição da lista

--- src/produtos.py
+++ src/produtos.py
@@ -1,8 +1,8 @@
 class Produto:
-    def __init__(self, id_bebida, marca, nome, estoque, preco, validade):
+    def __init__(self, id_bebida, nome, marca, estoque, preco, validade):
         self.id_bebida = id_bebida
-        self.marca = marca
         self.nome = nome
+        self.marca = marca
         self.estoque = estoque
         self.preco = preco
         self.validade = validade
